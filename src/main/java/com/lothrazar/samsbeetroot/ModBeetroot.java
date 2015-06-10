25c2c0f4f19b5d9efca9f084e7e8459e197d8f85
sam bassett <samson.bassett@gmail.com>
2015-06-10 10:11:38 -0700

    clean and fix mc info files. remove dead or nonfitting features

--- src/main/java/com/lothrazar/samsbeetroot/ModBeetroot.java
+++ src/main/java/com/lothrazar/samsbeetroot/ModBeetroot.java
@@ -17,12 +17,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = ModBeetroot.MODID, version = ModBeetroot.VERSION)
+@Mod(modid = ModBeetroot.MODID, useMetadata=true )
 public class ModBeetroot
 {
     public static final String MODID = "samsbeetroot";
-	public static final String TEXTURE_LOCATION = MODID + ":";
-    public static final String VERSION = "1.8-1.0.0"; 
+	public static final String TEXTURE_LOCATION = MODID + ":"; 
+	
 	@Instance(value = MODID)
 	public static ModBeetroot instance;	
 	@SidedProxy(clientSide="com.lothrazar.samsbeetroot.ClientProxy", serverSide="com.lothrazar.samsbeetroot.CommonProxy")
